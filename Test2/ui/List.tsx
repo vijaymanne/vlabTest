f990ba6f1857e36457e28d38c6bbd5f5e54b6154
vijaymanne <mvijaykumar.uid@gmail.com>
2024-12-20 14:17:53 +0530

    Update List.tsx

--- Test2/ui/List.tsx
+++ Test2/ui/List.tsx
@@ -1,4 +1,17 @@
 import React from "react";
 
+interface ListProps {
+  items: string[]; // List of item names
+}
+
+const List: React.FC<ListProps> = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default List;
