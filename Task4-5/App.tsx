6e0461f3ada6be2112424e3e4347b18724e7c848
vijaymanne <mvijaykumar.uid@gmail.com>
2024-12-20 14:37:58 +0530

    task 4 and 5

---
+++ Task4-5/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PokemonList from "./PokemonList";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <PokemonList />
+    </div>
+  );
+};
+
+export default App;
